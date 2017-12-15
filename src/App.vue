e42a49815e508e0f4efc11003384696b1d21d5e5
Yasushi Harada <chimanaco@gmail.com>
2017-12-15 00:52:45 -0800

    Add date and remove digital clock component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
   <div id="app">
-    <Clock :blink="true" />
+  <div id="clock">
+    <p class="date">{{ date }}</p>
+    <p class="time">{{ time }}</p>
+  </div>
   </div>
 </template>
 
 <script>
-import Clock from 'vue-digital-clock';
+const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 export default {
   name: 'app',
   components: {
-    Clock,
+  },
+  data() {
+    return {
+      time: '',
+      date: '',
+      timeID: '',
+    };
+  },
+  created() {
+    // this.updateTime();
+    this.timeID = setInterval(this.updateTime, 1000);
+  },
+  methods: {
+    zeroPadding(num, digit) {
+      let zero = '';
+      let i;
+      for (i = 0; i < digit; i += 1) {
+        zero += '0';
+      }
+      return (zero + num).slice(-digit);
+    },
+    updateTime() {
+      const cd = new Date();
+      this.time = `${this.zeroPadding(cd.getHours(), 2)}:${this.zeroPadding(cd.getMinutes(), 2)}:${this.zeroPadding(cd.getSeconds(), 2)}`;
+      this.date = `${this.zeroPadding(cd.getFullYear(), 4)}-${this.zeroPadding(cd.getMonth() + 1, 2)}-${this.zeroPadding(cd.getDate(), 2)} ${week[cd.getDay()]}`;
+    },
   },
 };
+
 </script>
 
 <style>
@@ -34,4 +63,15 @@ body {
   font-size: 200px;
   text-align: center;
 }
+
+p {
+  margin: 0;
+  padding: 0;
+  line-height: 1;
+}
+
+p.date {
+  font-size: 110px;
+}
+
 </style>
