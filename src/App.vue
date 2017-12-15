97694303d59e422aff5e2bc1d3c4058b3063f3b5
Yasushi Harada <chimanaco@gmail.com>
2017-12-14 23:12:05 -0800

    Init from vue.cli and add a clock

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Clock :blink="true" />
+  </div>
+</template>
+
+<script>
+import Clock from 'vue-digital-clock';
+
+export default {
+  name: 'app',
+  components: {
+    Clock,
+  },
+};
+</script>
+
+<style>
+body {
+  width: 100%;
+  height: 100%;
+  background: #000;
+  color: #FFF;
+}
+
+#app {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 200px;
+  text-align: center;
+}
+</style>
