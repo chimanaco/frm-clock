c843acdd68a53d3db2b49b8468db98f8d80e6736
Yasushi Harada <chimanaco@gmail.com>
2017-12-16 22:09:32 -0800

    Now has 3 clocks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <WorldClock v-bind:id="item" v-bind:zone="zone"></WorldClock>
-    <WorldClock v-bind:id="item2" v-bind:zone="zone2"></WorldClock>
+    <WorldClock v-bind:city="cities[0]" v-bind:zone="zones[0]"></WorldClock>
+    <WorldClock v-bind:city="cities[1]" v-bind:zone="zones[1]"></WorldClock>
+    <WorldClock v-bind:city="cities[2]" v-bind:zone="zones[2]"></WorldClock>
   </div>
 </template>
 
@@ -15,10 +16,8 @@ export default {
   },
   data() {
     return {
-      item: 'Tokyo',
-      zone: 9,
-      item2: 'Vancouver',
-      zone2: -8,
+      cities: ['VANCOUVER / CANADA', 'TOKYO / JAPAN', 'AMSTERDAM / NETHERLANDS'],
+      zones: [-8, 9, 1],
     };
   },
 };
@@ -32,20 +31,15 @@ body {
   width: 100%;
   height: 100%;
   background: #000;
-  color: #FFF;
-  //font-size: 50px;
-}
-
-#app {
-  // position: absolute;
-  width: 100%;
-  // top: 50%;
-  // transform: translate(0, -50%);
   font-family: Helvetica, Arial, sans-serif;
   // font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  font-size: 200px;
+  color: #FFF;
+}
+
+#app {
+  width: 100%;
   text-align: center;
 }
 
@@ -53,10 +47,6 @@ p {
   margin: 0;
   padding: 0;
   line-height: 1;
-}
-
-p.date, p.city {
-  font-size: 110px;
 }
 
 </style>
--- src/components/WorldClock.vue
+++ src/components/WorldClock.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="clockWrapper">
     <div class="clock">
-      <p class="date">{{ date }}</p>
+      <p class="city">{{ city }}</p>
       <p class="time">{{ time }}</p>
-      <p class="city">{{id}}</p>
+      <p class="date">{{ date }}</p>
     </div>
   </div>
 </template>
 
 <script>
-const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const month = ['January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'];
 export default {
   name: 'WorldClock',
-  props: ['id', 'zone'],
+  props: ['city', 'zone'],
   data() {
     return {
       time: '',
@@ -22,7 +24,6 @@ export default {
     };
   },
   created() {
-    // this.updateTime();
     this.timeID = setInterval(this.updateTime, 1000);
   },
   methods: {
@@ -45,11 +46,12 @@ export default {
       return `${hh}:${mm}:${ss}`;
     },
     dateToDateString(dt) {
-      const year = `${dt.getUTCFullYear()}`;
-      const month = `${this.zeroPadding(dt.getUTCMonth() + 1, 2)}`;
-      const date = `${this.zeroPadding(dt.getUTCDate(), 2)}`;
+      // const yy = `${dt.getUTCFullYear()}`;
+      // const mm = `${this.zeroPadding(dt.getUTCMonth() + 1, 2)}`;
+      const mm = `${month[dt.getUTCMonth()]}`;
+      const dd = `${this.zeroPadding(dt.getUTCDate(), 2)}`;
       const ww = `${week[dt.getUTCDay()]}`;
-      return `${year}/${month}/${date} ${ww}`;
+      return `${ww} ${dd} ${mm}`;
     },
     showDate(timeZone) {
       const tzTime = new Date(Date.now() + (timeZone * 3600000));
@@ -65,11 +67,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .clockWrapper {
   position: relative;
-  // background: blue;
-  height: 960px;
+  height: 640px;
+  //background-color: #287BD7;
 }
 
 .clock {
@@ -80,13 +81,19 @@ export default {
   transform: translate(50%, -50%);
 }
 
-p {
-  margin: 0;
-  padding: 0;
-  line-height: 1;
+p.time {
+  font-weight: bold;
+  font-size: 200px;
 }
 
-p.date, p.city {
-  font-size: 110px;
+p.date {
+  font-size: 75px;
+  //color: #CCC;
+}
+
+p.city {
+  margin-bottom: 10px;
+  font-weight: bold;
+  font-size: 65px;
 }
 </style>
